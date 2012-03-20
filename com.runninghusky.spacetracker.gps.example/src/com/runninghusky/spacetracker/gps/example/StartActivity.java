dcfb99ff3bd5b428f874ced6b2b06272084f8e79
Craig <craig.isakson@gmail.com>
2012-03-20 09:46:15 -0500

    Updated comments

--- com.runninghusky.spacetracker.gps.example/src/com/runninghusky/spacetracker/gps/example/StartActivity.java
+++ com.runninghusky.spacetracker.gps.example/src/com/runninghusky/spacetracker/gps/example/StartActivity.java
@@ -18,26 +18,59 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/**
+ * The Class StartActivity.
+ */
 public class StartActivity extends Activity {
+
+	/** The m loc manager. */
 	private LocationManager mLocManager;
+
+	/** The m loc listener. */
 	private LocationListener mLocListener;
+
+	/** The m distance. */
 	private TextView mLongitude, mLatitude, mAltitude, mSpeed, mTime,
 			mAccuracy, mBearing, mProvider, mDistance;
+
+	/** The m execute. */
 	private Button mExecute;
+
+	/** The m update interval. */
 	private EditText mUpdateInterval;
+
+	/** The is logging. */
 	private Boolean isLogging = false;
+
+	/** The ctx. */
 	private Context ctx = this;
+
+	/** The distance. */
 	private float distance = 0;
+
+	/** The old loc. */
 	private Location oldLoc;
+
+	/** The first run. */
 	private Boolean firstRun = true;
 
-	/** Allows the orientation to change without disrupting the activity */
+	/**
+	 * Allows the orientation to change without disrupting the activity.
+	 * 
+	 * @param newConfig
+	 *            the new config
+	 */
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
 	}
 
-	/** Called when the activity is first created. */
+	/**
+	 * Called when the activity is first created.
+	 * 
+	 * @param savedInstanceState
+	 *            the saved instance state
+	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -68,6 +101,9 @@ public class StartActivity extends Activity {
 
 	}
 
+	/**
+	 * Start logging.
+	 */
 	private void startLogging() {
 		try {
 			mLocListener = new OurLocationListener();
@@ -84,6 +120,9 @@ public class StartActivity extends Activity {
 		}
 	}
 
+	/**
+	 * Stop logging.
+	 */
 	private void stopLogging() {
 		try {
 			if (mLocManager != null) {
@@ -97,7 +136,36 @@ public class StartActivity extends Activity {
 		}
 	}
 
+	/**
+	 * Performs operations on destroy of activity
+	 */
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (mLocManager != null) {
+			if (mLocListener != null) {
+				mLocManager.removeUpdates(mLocListener);
+			}
+			mLocManager = null;
+		}
+	}
+
+	/**
+	 * The listener interface for receiving ourLocation events. The class that
+	 * is interested in processing a ourLocation event implements this
+	 * interface, and the object created with that class is registered with a
+	 * component using the component's
+	 * <code>addOurLocationListener<code> method. When
+	 * the ourLocation event occurs, that object's appropriate
+	 * method is invoked.
+	 * 
+	 * @see OurLocationEvent
+	 */
 	public class OurLocationListener implements LocationListener {
+
+		/**
+		 * onLocationChanged fires when the GPS/provider updates its loc
+		 */
 		@Override
 		public void onLocationChanged(Location loc) {
 			if (firstRun) {
@@ -113,14 +181,11 @@ public class StartActivity extends Activity {
 					.setText("Latitude:  " + String.valueOf(loc.getLatitude()));
 			mAltitude
 					.setText("Altitude:  "
-							+ String
-									.valueOf(roundTwoDecimals(loc.getAltitude() * 3.2808399))
+							+ String.valueOf(roundTwoDecimals(loc.getAltitude() * 3.2808399))
 							+ "ft above sea level");
-			mSpeed
-					.setText("Speed:  "
-							+ String
-									.valueOf(roundTwoDecimals(loc.getSpeed() * 2.23693629))
-							+ " mph");
+			mSpeed.setText("Speed:  "
+					+ String.valueOf(roundTwoDecimals(loc.getSpeed() * 2.23693629))
+					+ " mph");
 
 			Date d = new Date(loc.getTime());
 			SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
@@ -133,27 +198,42 @@ public class StartActivity extends Activity {
 					.setText("Provider:  " + String.valueOf(loc.getProvider()));
 			mDistance
 					.setText("Distance:  "
-							+ String
-									.valueOf(roundTwoDecimals(distance * 0.000621371192)));
+							+ String.valueOf(roundTwoDecimals(distance * 0.000621371192)));
 			oldLoc = loc;
 		}
 
+		/**
+		 * onProviderDisabled fires when the GPS/provider gets disabled
+		 */
 		@Override
 		public void onProviderDisabled(String arg0) {
 			Toast.makeText(ctx, "Provider Disabled", Toast.LENGTH_SHORT).show();
 		}
 
+		/**
+		 * onProviderEnabled fires when the GPS/provider is enabled
+		 */
 		@Override
 		public void onProviderEnabled(String arg0) {
 			Toast.makeText(ctx, "Provider Enabled", Toast.LENGTH_SHORT).show();
 		}
 
+		/**
+		 * onStatusChanged fires when the GPS/provider status changes
+		 */
 		@Override
 		public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
 			Toast.makeText(ctx, "Status Changed", Toast.LENGTH_SHORT).show();
 		}
 	}
 
+	/**
+	 * Round two decimals.
+	 * 
+	 * @param d
+	 *            the d
+	 * @return the double
+	 */
 	private double roundTwoDecimals(double d) {
 		try {
 			DecimalFormat twoDForm = new DecimalFormat("#.##");
@@ -167,17 +247,6 @@ public class StartActivity extends Activity {
 			} catch (NumberFormatException ne) {
 				return d;
 			}
-		}
-	}
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		if (mLocManager != null) {
-			if (mLocListener != null) {
-				mLocManager.removeUpdates(mLocListener);
-			}
-			mLocManager = null;
 		}
 	}
 }
